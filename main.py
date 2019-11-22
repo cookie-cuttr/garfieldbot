acf06f1d3e1d125f2d8fba06591334c9a9392c05
cookie-cuttr <55653866+cookie-cuttr@users.noreply.github.com>
2019-11-22 17:33:47 +0000

    Update main.py
    added a quick json selection thing so we don't have to shut down the bot every time we want to add trivia

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import discord
 import random
 import string
+import json
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="garf")
@@ -14,34 +15,36 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     msg = message.content.split()
-    chn = message.channel()
+    chn = message.channel
     
-    if message.author == bot.user or msg[0] != "garf":
+    if message.author == bot.user or msg == []:
         return
-
-    if msg[1] == "daily":
-        localtime = time.gmtime()
-        if len(str(localtime.tm_mday)) == 1:
-            day = "0" + str(localtime.tm_mday)
-        else:
-            day = str(localtime.tm_mday)
-        month = str(localtime.tm_mon)
-        year = str(localtime.tm_year)
-        curl = "https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/2019/{}-{}-{}.gif".format(year,month,day)
-
-        mbd = discord.Embed(image=curl)
-        await chn.send(embed=mbd)
-        
-    elif msg[1] == "trivia":
-        await chn.send("This feature isn't working yet, sorry for the inconvenience.")
-    elif msg[1] == "help":
-        await chn.send("This feature isn't working yet, sorry for the inconvenience.")
-    elif msg[1] == "links":
-        await chn.send("This feature isn't working yet, sorry for the inconvenience.")
-    elif msg[1] == "prompt":
-        await chn.send("This feature isn't working yet, sorry for the inconvenience.")
-    elif msg[1] == "comic":
-        await chn.send("This feature isn't working yet, sorry for the inconvenience.")
+    if msg[0] == "garf":
+        if msg[1] == "daily":
+            localtime = time.gmtime()
+            if len(str(localtime.tm_mday)) == 1:
+                day = "0" + str(localtime.tm_mday)
+            else:
+                day = str(localtime.tm_mday)
+            month = str(localtime.tm_mon)
+            year = str(localtime.tm_year)
+            curl = "https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/2019/{}-{}-{}.gif".format(year,month,day)
+            await chn.send(curl)
+            
+        elif msg[1] == "trivia":
+            with open('lst.json') as file:
+                data = json.load(file)
+                ran = random.choice(data['trivia'])['trivia']
+                await chn.send(ran)
+            
+        elif msg[1] == "help":
+            await chn.send("This feature isn't working yet, sorry for the inconvenience.")
+        elif msg[1] == "links":
+            await chn.send("This feature isn't working yet, sorry for the inconvenience.")
+        elif msg[1] == "prompt":
+            await chn.send("This feature isn't working yet, sorry for the inconvenience.")
+        elif msg[1] == "comic":
+            await chn.send("This feature isn't working yet, sorry for the inconvenience.")
     
 token = ""
 bot.run(token)
